9ac8cc381d3830ac5f25c59e6ed5da8117f6ac3d
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2025-01-14 00:51:16 +0100

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Container, Filters, Title } from "@/components/shared";
 import { TopBar } from "@/components/shared";
+import { ProductCard } from "@/components/shared/product-card";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -11,14 +13,45 @@ export default function Home() {
       <TopBar />
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           {/* Filtration */}
           <div className="w-[250px]">
             <Filters />
           </div>
 
           {/* List of products */}
-          <div className="flex flex-col gap-16">Product list</div>
+          <div className="flex flex-col gap-16">
+            <ProductsGroupList
+              title="Pizzas"
+              items={[
+                {
+                  id: 1,
+                  name: "Margherita",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:292x292/11ee7d6134bc4150bdd8e792d866ab52.avif",
+                  price: 23,
+                  items: [{ price: 23 }],
+                },
+                {
+                  id: 1,
+                  name: "Margherita",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:292x292/11ee7d6134bc4150bdd8e792d866ab52.avif",
+                  price: 23,
+                  items: [{ price: 23 }],
+                },
+                {
+                  id: 1,
+                  name: "Margherita",
+                  imageUrl:
+                    "https://media.dodostatic.net/image/r:292x292/11ee7d6134bc4150bdd8e792d866ab52.avif",
+                  price: 23,
+                  items: [{ price: 23 }],
+                },
+              ]}
+              categoryId={1}
+            />
+          </div>
         </div>
       </Container>
     </>
