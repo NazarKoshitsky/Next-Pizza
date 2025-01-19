b4ae93aeaa5cadaf5bef4c705065270b4c705389
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2025-01-19 14:48:32 +0100

    up

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from "./container";
 import Image from "next/image";
 import { Button } from "../ui";
 import { User, ShoppingCart, ArrowRight } from "lucide-react";
+import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 interface Props {
   className?: string;
@@ -15,14 +17,20 @@ export const Header: React.FC<Props> = ({ className }) => {
       <Container className="flex items-center justify-between py-8">
         {/* Left side */}
 
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="Logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase  font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">
-              It can't get any tastier
-            </p>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="Logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase  font-black">Next Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">
+                It can't get any tastier
+              </p>
+            </div>
           </div>
+        </Link>
+
+        <div className="mx-10 flex-1">
+          <SearchInput />
         </div>
 
         {/* Right side */}
