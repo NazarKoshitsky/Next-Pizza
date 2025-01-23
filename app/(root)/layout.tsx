5b5d518d40b3434458e74a591e9216c99615e678
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2025-01-23 22:20:08 +0100

    Creating modal

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,36 +1,21 @@
-import { Nunito } from "next/font/google";
 import { Metadata } from "next";
 
-import "../globals.css";
 import { Header } from "@/components/shared/header";
-
-const nunito = Nunito({
-  subsets: ["cyrillic"],
-  variable: "--font-nunito",
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "Next Pizza",
   description: "Order Pizza",
 };
 
-export default function RootLayout({
+export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link data-rh="true" rel="icon" href="/logo.png" />
-      </head>
-      <body className={nunito.className}>
-        <main>
-          <Header />
-          {children}
-        </main>
-      </body>
-    </html>
+    <main>
+      <Header />
+      {children}
+    </main>
   );
 }
